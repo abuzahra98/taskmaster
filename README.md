4d97a80849c58d33e7b3f10bb7625b67155b2c44
abuzahra98 <dawoodbasher@gmail.com>
2021-09-14 19:15:58 +0300

    edit

--- README.md
+++ README.md
@@ -27,4 +27,6 @@ Each of the buttons displays a different taskItem title, and displays that title
 ![image description](screenshots/14.png)
 ![image description](screenshots/15.png)
 ![image description](screenshots/17.png)
-![image description](screenshots/aa.png)
+![image description](screenshots/asd.png)
+![image description](screenshots/18.png)
+![image description](screenshots/19.png)
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -24,6 +24,7 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
 import com.amplifyframework.datastore.generated.model.Todo;
@@ -41,16 +42,27 @@ public class MainActivity extends AppCompatActivity  {
 
         setContentView(R.layout.activity_main);
 
+
+
+
         try {
+
             Amplify.addPlugin(new AWSApiPlugin()); // UNCOMMENT this line once backend is deployed
             Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+
             Amplify.configure(getApplicationContext());
 
+
+            Amplify.Auth.signInWithWebUI(
+                    this,
+                    result -> Log.i("AuthQuickStart", result.toString()),
+                    error -> Log.e("AuthQuickStart", error.toString())
+            );
             Log.i("MyAmplifyApp", "Initialized Amplify");
         } catch (AmplifyException error) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
         }
-
 
 //          appDatabase = Room.databaseBuilder(getApplicationContext(),AppDatabase.class,"ddd").allowMainThreadQueries().build();
 //        List<Todo> taskList = appDatabase.taskDao().getAll();
@@ -115,11 +127,21 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
 
-//        ArrayList<Task> allTask = new ArrayList<Task>();
-//        allTask.add(new Task("Football","      Several codes of football. Left to right, top to bottom: association, gridiron, Australian rules, rugby union, rugby league and Gaelic Football is a family of team sports that involve, to varying degrees, kicking a ball to score a goal. Unqualified, the word football normally means the form of football that is the most popular where the word is used. Sports commonly called football include association football (known as soccer in North America and Oceania); gridiron football (specifically American football or Canadian football); Australian rules football; rugby union and rugby league; and Gaelic football.[1] These various forms of football share to varying extent common origins and are known as football codes.","in progress"));
-//        allTask.add(new Task("Basketball","Basketball is a team sport in which two teams, most commonly of five players each, opposing one another on a rectangular court, compete with the primary objective of shooting a basketball (approximately 9.4 inches (24 cm) in diameter) through the defender's hoop (a basket 18 inches (46 cm) in diameter mounted 10 feet (3.048 m) high to a backboard at each end of the court) while preventing the opposing team from shooting through their own hoop. A field goal is worth two points, unless made from behind the three-point line, when it is worth three. After a foul, timed play stops and the player fouled or designated to shoot a technical foul is given one, two or three one-point free throws.","assign"));
-//        allTask.add(new Task("Bowling","Bowling is a target sport and recreational activity in which a player rolls a ball toward pins (in pin bowling) or another target (in target bowling). The term bowling usually refers to pin bowling (most commonly ten-pin bowling), though in the United Kingdom and Commonwealth countries, bowling could also refer to target bowling, such as lawn bowls.","completed"));
 
+        Button signOut=findViewById(R.id.signout);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Amplify.Auth.signOut(
+                        () -> {
+
+                            Log.i("AuthQuickstart", "Signed out successfully");
+                        },
+                        error -> Log.e("AuthQuickstart", error.toString())
+                );
+            }
+
+        });
     }
 
     @Override
